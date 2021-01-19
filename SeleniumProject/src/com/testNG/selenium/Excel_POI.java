7385eb72a0bbf8945c518976b2de28fda73582a0
villu <villu@LAPTOP-LG2N16NC>
2021-01-19 11:58:11 +0530

    update

--- SeleniumProject/src/com/testNG/selenium/Excel_POI.java
+++ SeleniumProject/src/com/testNG/selenium/Excel_POI.java
@@ -1,6 +1,6 @@
 package com.testNG.selenium;
 
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
@@ -9,13 +9,14 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class Excel_POI {
 
 		public static void main(String[] args) throws IOException {
 	        String excelFilePath = "C:\\Users\\villu\\Documents\\TestData_JXL.xls";
-	        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+	        FileInputStream inputStream = new FileInputStream(excelFilePath);
 	        Workbook workbook = new HSSFWorkbook(inputStream);
 	        Sheet firstSheet = workbook.getSheetAt(0);
 	        Iterator<Row> iterator = firstSheet.iterator();
@@ -37,6 +38,8 @@ public class Excel_POI {
 	                        break;
 	                    case NUMERIC:
 	                        System.out.print(cell.getNumericCellValue()+"\t");
+	                        double num= cell.getNumericCellValue();
+	                        NumberToTextConverter.toText(num);
 	                        break;
 	                }
 	                
